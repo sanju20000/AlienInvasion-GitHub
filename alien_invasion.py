d7b057f9f4392613cb60616e9ce44ed448b73d0f
rahulkumawat <samsonsanju146@gamil.com>
2020-08-03 14:40:56 +0530

    made a simple window in pygame

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+
+import pygame
+
+def run_game():
+    # Initializes game and create a screen object.
+    pygame.init()
+    screen = pygame.display.set_mode((1200,600))
+
+    pygame.display.set_caption("Alien Invasion")
+
+    # start the main loop for the game
+    while True:
+
+        # watch for keyboards and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()
